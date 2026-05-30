06f0e56a914203fb8f3a07cdc0b9cf86f6550917
Ganesh <ganeshgarje716@gmail.com>
2026-05-30 23:13:35 +0530

    Create Custom Method find By name, city, marks, course, email

--- SpringBoot-5/src/main/java/com/ganesh/SpringBoot5Application.java
+++ SpringBoot-5/src/main/java/com/ganesh/SpringBoot5Application.java
@@ -1,13 +1,66 @@
 package com.ganesh;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ganesh.entity.Student;
+import com.ganesh.repository.StudentRepository;
+
 @SpringBootApplication
-public class SpringBoot5Application {
+public class SpringBoot5Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot5Application.class, args);
+	}
+
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+
+//		List<Student> name = studentRepository.findByName("Ganesh");
+//		
+//		name.forEach(i->System.err.println(i));
+		
+		
+//		List<Student> city = studentRepository.findByCity("mumbai");
+//		
+//		city.forEach(i->System.err.println(i));
+		
+		
+//		List<Student> marks = studentRepository.findByMarks(90.00);
+//		
+//		marks.forEach(i->System.err.println(i));
+		
+//		
+//		List<Student> course = studentRepository.findByCourse("IT");
+//		
+//		course.forEach(i->System.err.println(i));
+		
+		
+//		List<Student> email = studentRepository.findByEmail("ganesh@gmail.com");
+//		
+//		email.forEach(i->System.err.println(i));
+		
+		
+        List<Student> list = studentRepository.findByNameOrCity("Ganesh", "Pune");
+		
+		list.forEach(i->System.err.println(i));
+		
+		
+		
+		
+		
 	}
 
 }
---
+++ SpringBoot-5/src/main/java/com/ganesh/repository/StudentRepository.java
@@ -0,0 +1,26 @@
+package com.ganesh.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ganesh.entity.Student;
+
+
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+	
+	
+	
+	List<Student> findByName(String name);
+	
+	List<Student> findByCity(String city);
+	
+	List<Student> findByMarks(Double marks);
+
+	List<Student> findByCourse(String course);
+	
+	List<Student> findByEmail(String email);
+	
+	List<Student> findByNameOrCity(String name, String city);
+
+}
